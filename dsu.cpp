b2a58fdc6fa198a37cafc239216d66f4d583e896
ThomaFM <72629297+ThomaFM@users.noreply.github.com>
2022-11-03 14:17:00 -0400

    Add files via upload
    Adding Disjoint Set Union, Union by size so size information can be queried.

---
+++ dsu.cpp
@@ -0,0 +1,27 @@
+struct dsu{
+    vector<int> parent;
+    vector<int> siz;
+    dsu(int n){
+        siz = vector<int>(n,1);
+        for(int i = 0; i < n; i++)
+            parent.push_back(i);
+    }
+    int find(int a){
+        if(parent[a] != a) parent[a] = find(parent[a]);
+        return parent[a];
+    }
+    bool join(int a, int b){
+        a = find(a);
+        b = find(b);
+        if(a == b) return false;
+        if(siz[a] >= siz[b]){
+            parent[b] = a;
+            siz[a]+=siz[b];
+            return true;
+        }else{
+            parent[a] = b;
+            siz[b]+=siz[a];
+            return true;
+        }
+    }
+};
