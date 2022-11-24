ba906752025eebf9b0c0a134a2575aaddad2e385
ThomaFM <72629297+ThomaFM@users.noreply.github.com>
2022-11-23 19:35:32 -0500

    prime seive

---
+++ seive.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+vector<bool> seive(int n){
+    vector<bool> ip(n+1,true);
+    ip[0] = ip[1] = false;
+    for(ll i = 2; i <= n; i++){
+        if(ip[i] && i*i <=n){
+            for(int j = i*i; j <= n; j+=i) ip[j] = false;
+        }
+    }
+    return ip;
+}
+
+//Usage
+//vector<int> isprime = seive(num);
