9facdebe37589642a0bff24b66e89d9f8cfd24c0
ThomaFM <72629297+ThomaFM@users.noreply.github.com>
2023-10-11 17:25:55 -0400

    Update dsu.cpp

--- dsu.cpp
+++ dsu.cpp
@@ -1,24 +1,18 @@
 struct dsu{
-    vector<int> parent;
-    vector<int> siz;
-    dsu(int n){
-        siz = vector<int>(n,1);
-        for(int i = 0; i < n; i++)
-            parent.push_back(i);
-    }
-    int find(int a){
-        if(parent[a] != a) parent[a] = find(parent[a]);
-        return parent[a];
-    }
-    bool join(int a, int b){
-        a = find(a);
-        b = find(b);
-        if(a == b) return false;
-        if(siz[a] < siz[b])
-            swap(a,b);
-        parent[b] = a;
-        siz[a]+=siz[b];
-        return true;
-
-    }
+   vector<int> par,siz;
+   dsu(int n){
+       siz = par = vector<int>(n,1);
+       iota(par.begin(),par.end(),0);
+   }
+   int find(int a){
+       if(par[a] != a) par[a] = find(par[a]);
+       return par[a];
+   }
+   bool join(int a, int b){
+       a = find(a); b = find(b);
+       if(a == b) return false;
+       if(siz[a] < siz[b]) swap(a,b);
+       par[b] = a; siz[a]+=siz[b];
+       return true;
+   }
 };
