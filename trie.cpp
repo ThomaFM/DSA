c00c08580cbaaff871dd364db3f769eb4fa0fea9
ThomaFM <72629297+ThomaFM@users.noreply.github.com>
2023-10-11 17:44:15 -0400

    Update trie.cpp

--- trie.cpp
+++ trie.cpp
@@ -1,25 +1,36 @@
-#include <bits/stdc++.h>
-using namespace std;
-//Code Assumes possible alphabet is continues sequence of ascii values
-//change -'a' to first letter of alphabet.
-const int ALPHASIZE = 26;
+const int ALPHASIZE = 26; //assumes alphabet is continues
 struct node {
-    bool isWord = false;
-    vector<node*> children;
-    node() {
-        children = vector<node*>(ALPHASIZE, nullptr);
-    }
+   bool isWord = false; //int ct for kth lex
+   vector<node*> children;
+   node() {
+       children = vector<node*>(ALPHASIZE, nullptr);
+   } //change -'a' to the first letter of the alphabet.
+   void insert(string &s, int idx) { //ct++ at top for kth lex
+       if(idx == s.size()) {isWord = true; return;}
+       if(!children[s[idx]-'a'])
+           children[s[idx]-'a'] = new node();
+       children[s[idx]-'a'] -> insert(s, idx+1);
+   }
 
-    void insert(string &s, int idx) {
-        if(idx == s.size()) {isWord = true; return;}
-        if(!children[s[idx]-'a'])
-            children[s[idx]-'a'] = new node();
-        children[s[idx]-'a'] -> insert(s, idx+1);
-    }
 
-    bool find(string &s, int idx) {
-        if(idx == s.size()) return isWord;
-        if(!children[s[idx]-'a']) return false;
-        return children[s[idx]-'a'] -> find(s, idx+1);
-    }
+   bool find(string &s, int idx) {
+       if(idx == s.size()) return isWord;
+       if(!children[s[idx]-'a']) return false;
+       return children[s[idx]-'a'] -> find(s, idx+1);
+   }
+//stop here if don’t need find kth 
+   void findkth(string &s, int k){
+       int sum = 0;
+       k-=isWord;
+       if(!k) return;
+       for(auto i: order){  //order of alphabet is 0,1,2-
+           if(children[i] == nullptr) continue;
+           if((sum+(children[i]->ct)) >= k){
+               s.push_back(i+'a');
+               children[i]->findkth(s,k-sum);
+               return;
+           }
+           sum+=(children[i]->ct);
+       }
+   }
 };
