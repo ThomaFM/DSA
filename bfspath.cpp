9757e446f1cc6a6a195982adea777e9fa84af0af
ThomaFM <72629297+ThomaFM@users.noreply.github.com>
2023-10-11 17:23:29 -0400

    Update bfspath.cpp

--- bfspath.cpp
+++ bfspath.cpp
@@ -1,31 +1,24 @@
 //Written by Thomas Meeks
 //BFS with Path Reconstruction - 0 Based
-//graph is adjecency list vector<vector<int>> 
+//graph is adjacency list vector<vector<int>> 
 //graph[u] = List of Nodes connected to u
 //nodes = #number of nodes in graph i.e graph.size()
-//source is source node
-
-vector<bool> vis(nodes);
-vector<int> prev(nodes,-1);
-queue<int> q;
-vis[source] = true;
-q.push(source);
-
+//source is the source node
+vector<int> dist(n,-1);
+vector<int> par(n,-1);
+queue<int> q; q.push(source);
+dist[source] = 0;
 while(!q.empty()){
-    int u = q.front(); q.pop();
-    for(auto v: graph[u]){
-        if(!vis[v]){
-            vis[v] = true;
-            prev[v] = u;
-            q.push(v);
-        }
-    }
+   int u = q.front(); q.pop();
+   for(auto v: graph[u]){
+       if(dist[v] != -1) continue;
+       dist[v] = dist[u] + 1;
+       par[v] = u; //Path Build back
+       q.push(v);
+   }
 }
 
+//Path build back, if path[0] != source NO PATH
 vector<int> path;
-for(int at = nodes-1; at != -1; at = prev[at]){
-    path.push_back(at);
-}
-
+for(int at = sink; at != -1; at = par[at]) path.push_back(at);
 reverse(path.begin(),path.end());
-//if path[0] != source NO PATH
